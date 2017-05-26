e3d7a29c7e0ed00d49425a5d46ffa9604c0fa029
Vidushi Vashishth <reachvidu@gmail.com>
2017-05-26 10:46:33 +0530

    Update tempimage.py

--- imageconfig/tempimage.py
+++ imageconfig/tempimage.py
@@ -3,7 +3,7 @@ import uuid
 import os
 
 class TempImage:
-	def __init__(self, frequency, basePath="./", ext=".jpg"):
+	def __init__(self, frequency, basePath="./imageconfig/images", ext=".jpg"):
 		# construct the file path
 		self.path = "{base_path}/{frequency}{ext}".format(base_path=basePath,
 			frequency = frequency ,ext=ext)
